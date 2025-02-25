115f85ce6a93f2ca1085d8f1dd589614568c08f7
abdelrahman <abdelrhaman72@gmail.com>
2025-02-25 16:44:37 +0200

    .

--- client/vite.config.js
+++ client/vite.config.js
@@ -5,7 +5,8 @@ import tailwindcss from '@tailwindcss/vite'
 export default defineConfig({
   plugins: [
     tailwindcss(),
-    react()
-    
+    react(),
+ 
   ],
+  base:"/Muharrq-Org-Demo",
 })
